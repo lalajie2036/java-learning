ba5dac25d0ca717c1578b577e5bf98b9499aae9f
lalajie <15862811804@163.com>
2020-10-05 19:06:51 +0800

    java-learning

---
+++ src/com/lalajie/java/A.java
@@ -0,0 +1,10 @@
+package com.lalajie.java;
+
+/**
+ * @ClassName A
+ * @Description TODO
+ * @Author Mister-Lu
+ * @Date 2020/10/5
+ **/
+public class A {
+}
---
+++ src/com/lalajie/java/week1/Factorial.java
@@ -0,0 +1,10 @@
+package com.lalajie.java.week1;
+
+/**
+ * @ClassName Factorial
+ * @Description TODO
+ * @Author Mister-Lu
+ * @Date 2020/10/5
+ **/
+public class Factorial {
+}
