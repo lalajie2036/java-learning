4f9683c03c36f6cdfae8a729e8a47214019f0f3e
lalajie <15862811804@163.com>
2020-10-05 21:02:03 +0800

    java-learning

--- src/com/lalajie/java/week1/ArrayTest.java
+++ src/com/lalajie/java/week1/ArrayTest.java
@@ -1,5 +1,5 @@
 package com.lalajie.java.week1;
-
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -10,6 +10,7 @@ import java.util.Random;
  **/
 public class ArrayTest {
     static int MAX = 5;
+    static int sum = 0;
     public static void main(String[] args) {
         int[] a = new int[MAX];
         Random random = new Random();
@@ -17,5 +18,44 @@ public class ArrayTest {
             a[i] = random.nextInt(100);
             System.out.print(a[i] + " ");
         }
+        System.out.println();
+        System.out.print("排序：");
+
+        /**
+         * 对上面数排序
+         */
+        Arrays.sort(a);
+        for(int j = 0;j<a.length;j++){
+            System.out.print(a[j] + " ");
+        }
+
+        /**
+         * 求出数组和
+         */
+        int j;
+        for (j = 0;j<a.length;j++){
+            sum = sum + a[j];
+        }
+        System.out.println();
+        System.out.println("数组的和为： " + sum);
+        /**
+         * 求出最大值和最小值
+         */
+        int min=a[0];
+        int max=a[0];
+        for (int i = 1; i <a.length ; i++) {
+            if (a[i]>max){
+                max=a[i];
+            }
+            if (a[i]<min){
+                min=a[i];
+            }
+
+        }
+        System.out.println();
+        System.out.print("数组的最大值为:"+max);
+        System.out.print("\n数组的最小值为:"+min);
+
     }
+
 }
