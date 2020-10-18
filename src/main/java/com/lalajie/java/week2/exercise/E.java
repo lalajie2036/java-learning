f845f3272ee55b0321ec54aa4ff8b0205da368a5
lalajie <15862811804@163.com>
2020-10-18 17:14:16 +0800

    java-learning完善

---
+++ src/main/java/com/lalajie/java/week2/exercise/E.java
@@ -0,0 +1,35 @@
+package com.lalajie.java.week2.exercise;
+
+/**
+ * @ClassName Work
+ * @Description TODO
+ * @Author Mister-Lu
+ * @Date 2020/10/18
+ **/
+//class Lake{
+//    Fish fish;
+//    void setFish(Fish s){
+//        fish = s;
+//    }
+//    void foodFish(int m){
+//        fish.weight = fish.weight + m;
+//    }
+//    Fish getFish(){
+//        fish.weight -= 10;
+//        return fish;
+//    }
+//}
+//
+//public class E {
+//    public static void main(String[] args) {
+//        Fish redFish = new Fish();
+//        Lake lake = new Lake();
+//        lake.setFish(redFish);
+//        lake.foodFish(120);
+//        int w = lake.fish.weight;
+//        Fish fishOK = lake.getFish();
+//        System.out.printf(" %d: %d: %d:",w,lake.fish.weight,fishOK.weight);
+//    }
+//}
+
+
